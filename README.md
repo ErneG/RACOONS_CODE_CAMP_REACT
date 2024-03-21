83a6920bd9415d44287056d68bc0bf17cba0027c
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2024-03-22 00:09:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# The Raccoons Code Camp 6 - Javascript and React
+
+## Check dependencies
+
+```bash
+node -v  // Check Node.js installation
+npm -v   // Check Node.js installation
+git -v   // Check Node.js installation
+```
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+    return (
+        <main className="h-screen w-full">
+            <h1>
+                Congratulations! You successfully launched
+                this React project!
+            </h1>
+        </main>
+    );
+};
+
+export default App;
