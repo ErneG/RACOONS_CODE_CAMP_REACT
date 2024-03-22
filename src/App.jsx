6c7db9a14b3fa9f2f1b1f467d62763fe2970e700
GrumpySquiggle <46605904+SolidDeath@users.noreply.github.com>
2024-03-22 03:56:52 +0200

    Dynamically generated boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 const App = () => {
+    const values = ['BOX 1', 'BOX 2']; // An array of values
+
     return (
         <main className="h-screen w-full">
             <div>
-                <div
-                    className="inline-flex transform items-center justify-center rounded-lg
-            border-2 border-solid border-gray-700 p-2 transition hover:scale-105"
-                >
-                    BOX 1
-                </div>
+                {values.map((value, index) => (
+                    <div
+                        key={index} // Necessary for React to keep track of the elements
+                        className="inline-flex transform items-center justify-center rounded-lg border-2 border-solid border-gray-700 p-2 transition hover:scale-105"
+                    >
+                        {value}
+                    </div>
+                ))}
             </div>
         </main>
     );
